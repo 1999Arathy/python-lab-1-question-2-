05abe934189eaed44e9d63200fbf333aed970b01
1999Arathy <85410988+1999Arathy@users.noreply.github.com>
2023-06-10 12:27:25 +0530

    Update Lab-1_ques-2.py

--- Lab-1_ques-2.py
+++ Lab-1_ques-2.py
@@ -1,13 +1,7 @@
-#Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right
+#Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
 
-import random
-
-random_number = random.randrange(0,9)
-x = int(random_number+1)
-user_guess = int(input("guess a number between 1 and 10"))
-if(x == user_guess):
-    print("You gussed exactly correct")
-elif(x > user_guess):
-    print("You gussed a lower number")
+user_input = int(input("enter a number"))
+if(user_input %2 ==0):
+    print("User input is an even number")
 else:
-    print("You gussed greater value")
+    print("User input is an odd number")
